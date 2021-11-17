4d12d79f35df94789b51c6d027e5c23ecb2e29b6
pirat-droid <aldegar80@gmail.com>
2021-11-17 23:18:20 +0500

    Телеграм бот для канала VPROKino

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from create_db import CreateDB
 from network import Internet
+from parsing import Parsing
+import requests
 
 db = CreateDB('message.sqlite3')
 db.create_connect()
 
 if not Internet('https://ya.ru/').check():
     print('Internet connect Lost!')
+    exit()
 
-# parsing site
-# https://www.kinoafisha.info/movies/ в прокате сейчас, брать раз в три дня, парсинг
-# https://www.kinopoisk.ru/premiere/ru/2021/month/11/ премьеры каждый день, парсинг
+premieres_today = Parsing('https://www.kinoafisha.info/movies/')
+title_t = premieres_today.get_data('span', 'movieItem_title')
+subtitle_t = premieres_today.get_data('span', 'movieItem_subtitle')
+genres_t = premieres_today.get_data('span', 'movieItem_genres')
+country_t = premieres_today.get_data('span', 'movieItem_year')
 
+print(title_t)
+print(subtitle_t)
+print(genres_t)
+print(country_t)
 
+premieres_month = Parsing('https://www.kinoafisha.info/releases/2021/11/')
+title_m = premieres_month.get_data('span', 'movieItem_title')
+subtitle_m = premieres_month.get_data('span', 'movieItem_subtitle')
+genres_m = premieres_month.get_data('span', 'movieItem_genres')
+country_m = premieres_month.get_data('span', 'movieItem_year')
+
+print(title_m)
+print(subtitle_m)
+print(genres_m)
+print(country_m)
+
+# убрать спец символы
+news = Parsing('https://www.kinomania.ru/news')
+p = news.get_data('div', 'news-pagelist-item-content')
+print(p)
+a = news.get_data('div', 'pagelist-item', True)
+print(a)
--- parsing.py
+++ parsing.py
@@ -1,7 +1,24 @@
+import requests
 from bs4 import BeautifulSoup
 
 
-class ParsingPost:
+class Parsing:
 
-    def __init__(self, page):
-        self.soup = BeautifulSoup(page, 'html.parser')
+    def __init__(self, link):
+        try:
+            req = requests.get(link)
+        except:
+            print('Can not open link %s' % link)
+            exit()
+        self.soup = BeautifulSoup(req.text, 'html.parser')
+
+    def get_data(self, tag, class_name, img=False):
+        elements = self.soup.find_all(tag, class_name)
+        data = []
+        for element in elements:
+            if img:
+                # print(element)
+                data.append(element.img['data-original'])
+            else:
+                data.append(element.get_text())
+        return data
