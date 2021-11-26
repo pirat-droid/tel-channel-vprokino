c8cb106d209f2cd1f8c7ef1b85abf186492c0d61
pirat-droid <aldegar80@gmail.com>
2021-11-27 00:01:32 +0500

    Добавлена отправка изображений в телеграм канал.

--- telegram.py
+++ telegram.py
@@ -3,10 +3,14 @@ from authentication import *
 
 
 class Telegram:
-    def __init__(self, channel, message):
+    def __init__(self, channel, message, image=None):
         self.channel = channel
         self.message = message
         self.client = TelegramClient(phone, api_id, api_hash).start()
+        self.image = image
 
     def send_message(self):
-         self.client.send_message(self.channel, self.message)
+        if self.image is not None:
+            self.client.send_file(self.channel, self.image, caption=self.message)
+        else:
+            self.client.send_message(self.channel, self.message)
